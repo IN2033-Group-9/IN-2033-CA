0a9d697c11c7466e08d3e58a40de18569d7b0636
Dylan-byt <dylan.johnson@city.ac.uk>
2026-03-02 19:00:36 +0000

    Delete CA_OnlineOrderAPI.java
    moved code

--- CA_OnlineOrderAPI.java
+++
@@ -1,9 +0,0 @@
-public interface CA_OnlineOrderAPI {  
-
-    abstract void processOnlineOrder(String orderID, String basketOrder);
-
-    abstract int checkProductStock(String productID);
-
-    abstract String[] getMerchantCatalogue(String searchTerm);
-    
-}  
