5cea0d06952195ae3d545dd16ff2b9cf1d2833e5
Dylan-byt <dylan.johnson@city.ac.uk>
2026-03-02 19:10:20 +0000

    Create SA_ORD_API.java
    added the code

---
+++ scr/SA_ORD_API.java
@@ -0,0 +1,13 @@
+public interface SA_ORD_API {   
+
+    abstract String newOrder();  
+
+    abstract void addItems(String orderID, int[] itemIDs, int[] quantities);   
+
+    abstract void removeItems(String orderID, int[] itemIDs, int[] quantitiesToRemove);  
+
+    abstract void submitOrder(String orderID);  
+
+    abstract Catalogue getActiveCatalogue();  
+    
+}
