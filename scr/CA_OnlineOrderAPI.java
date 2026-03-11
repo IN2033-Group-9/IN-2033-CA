71ef490190f2080c260830da6335b1a5b519248d
Dylan-byt <dylan.johnson@city.ac.uk>
2026-03-11 15:03:32 +0000

    same as before fixing copy paste issues

--- scr/CA_OnlineOrderAPI.java
+++ scr/CA_OnlineOrderAPI.java
@@ -4,24 +4,24 @@ public interface CA_OnlineOrderAPI {
 
 
     /** 
-     * 
-     * @param orderId 
-     * @param basketOrder 
-     */
+    * 
+    * @param orderId 
+    * @param basketOrder 
+    */
 
     abstract void processOnlineOrder(String orderID, String basketOrder);
 
     /** 
-     * 
-     * @param productId 
-     */
+    * 
+    * @param productId 
+    */
 
     abstract int checkProductStock(String productID);
 
     /** 
-     * 
-     * @param searchTerm
-     */
+    * 
+    * @param searchTerm
+    */
 
     abstract String[] getMerchangtCatalogue(String searchTerm);
 }
--- scr/PU_COMMS_API.java
+++ scr/PU_COMMS_API.java
@@ -1,18 +1,12 @@
 
 public interface PU_COMMS_API {
     
-     /** 
-
+    /** 
     * 
-
     * @param recipient 
-
     * @param subject 
-
     * @param content 
-
     */ 
-
     abstract boolean sendEmail(String recipient, String subject, String content); 
 
 } 
