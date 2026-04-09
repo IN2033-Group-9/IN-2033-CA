566f032a1aed3f05c930bbb3548c49a1f9159dfc
Reiny-2000 <Reinhardas.Girdziusas@city.ac.uk>
2026-04-09 05:18:32 +0100

    Add files via upload
    Updated interface

---
+++ Main.java
@@ -0,0 +1,21 @@
+
+
+public interface PU_COMMS_API {
+
+    /**
+     * Sends card payment details securely to PU subsystem.
+     * @param cardNumber Full card number (temporarily, must be encrypted in transit)
+     * @param expiry Expiry date (MM/YY)
+     * @param amount Payment amount
+     * @param orderID Associated order ID
+     * @return true if PU confirms successful processing, false otherwise
+     *
+     * @param recipient
+     * @param subject
+     * @param content
+     */
+    boolean processCardPayment(String cardNumber, String expiry, double amount, String orderID);
+
+    abstract boolean sendEmail(String recipient, String subject, String content);
+
+}
